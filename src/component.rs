bef73b1b3d330d0522a1f315720d8e3552a4b105
u-kai <76635578+u-kai@users.noreply.github.com>
2024-04-18 22:40:06 +0900

    add components

--- src/component.rs
+++ src/component.rs
@@ -2,21 +2,91 @@ use std::collections::HashMap;
 
 pub(super) struct Component {
     pub name: String,
-    props: Props,
+    props: NamedProps,
 }
 
-struct Props {
-    name: String,
+struct NamedProps {
+    pub name: String,
+    inner: Props,
+}
+
+impl NamedProps {
+    fn new(name: impl Into<String>, inner: Props) -> Self {
+        Self {
+            name: name.into(),
+            inner,
+        }
+    }
+    fn expand_str(&self) -> String {
+        self.inner.to_str()
+    }
+}
+
+pub(super) struct Props {
     inner: HashMap<Key, Type>,
 }
+
 impl Props {
-    fn expand_str(&self) -> String {
+    pub fn new() -> Self {
+        Self {
+            inner: HashMap::new(),
+        }
+    }
+    pub fn insert(&mut self, key: Key, ty: Type) {
+        self.inner.insert(key, ty);
+    }
+    fn to_str(&self) -> String {
         let mut props = String::new();
         for (key, ty) in &self.inner {
-            props.push_str(&format!("{}: {},", key.0, ty.0));
+            props.push_str(&format!("{}: {},", key.0, ty.to_str()));
         }
         format!("{{ {} }}", props)
     }
 }
-struct Key(String);
-struct Type(String);
+
+#[derive(Debug, Clone, PartialEq, Hash, Eq)]
+pub(super) struct Key(String);
+
+pub(super) enum Type {
+    Number,
+    String,
+    Boolean,
+    Array(Box<Type>),
+    Object(Props),
+    Original(String),
+}
+
+impl Type {
+    fn to_str(&self) -> String {
+        match self {
+            Type::Number => "number".to_string(),
+            Type::String => "string".to_string(),
+            Type::Boolean => "boolean".to_string(),
+            Type::Array(ty) => format!("{}[]", ty.to_str()),
+            Type::Object(props) => props.to_str(),
+            Type::Original(s) => s.clone(),
+        }
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_expand_str() {
+        let props = Props {
+            inner: vec![
+                (Key("timeOut".to_string()), Type::Number),
+                (Key("errorMessage".to_string()), Type::String),
+            ]
+            .into_iter()
+            .collect(),
+        };
+        assert!(
+            props.to_str() == "{ timeOut: number,errorMessage: string, }"
+                || props.to_str() == "{ errorMessage: string,timeOut: number, }"
+        );
+    }
+}
